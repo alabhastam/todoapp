1e3ab8680661ed12eaafb04a86872d0b642a5402
alabhastam <abdollahia713@gmail.com>
2024-08-29 05:15:48 -0700

    routing to tasks

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TodoController;
+use App\Http\Controllers\TaskController;
+
 
 Route::get('/', function () {
     return view('welcome');
 });
 
 Route::get('/todo', [TodoController::class, 'index']);
+Route::resource('tasks', TaskController::class);
